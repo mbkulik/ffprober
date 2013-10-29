6452ee7820e4e538c64a4acbe2e3e4f0e52e7c81
Michael B. Kulik <mike.b.kulik@gmail.com>
2013-10-29 12:18:14 -0400

    added support for ffmpeg/ffprobe version 2.1

--- spec/ffprober/ffprobe_version_spec.rb
+++ spec/ffprober/ffprobe_version_spec.rb
@@ -8,9 +8,10 @@ describe Ffprober::FfprobeVersion do
     { version: "1.1.0", pass: true },
     { version: "2.9.0", pass: false },
     { version: "1.2.1", pass: true },
-    { version: "2.0", pass: true},
+    { version: "2.0", 	pass: true},
     { version: "2.0.1", pass: true },
-    { version: "2.0.2", pass: true }
+    { version: "2.0.2", pass: true },
+    { version: "2.1",	pass: true }
   ]
 
   context 'validates the ffprobe version' do
